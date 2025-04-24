00e3d983cb8638f118e61c484e9ee584847aed3b
Petterini <ppetterini@gmail.com>
2025-04-24 11:57:07 -0300

    Feat: Adicionando front-end e spring security

---
+++ src/main/java/com/pedropetterini/votacaoctg/controllers/LoginViewController.java
@@ -0,0 +1,13 @@
+package com.pedropetterini.votacaoctg.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String paginaLogin() {
+        return "login";
+    }
+}
